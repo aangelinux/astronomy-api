00738cc1658d55639ad115f15b162ab4870f1d75
Angelina Larsson <al227bn@student.lnu.se>
2026-02-25 17:27:33 +0100

    Add secondary resources to seed script

--- scripts/seed.js
+++ scripts/seed.js
@@ -12,83 +12,135 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const config = {
-	table: 'Close_Approaches',
-	filePath: path.join(__dirname, '../scripts/neo_close_approaches.csv'),
+const PRIMARY = {
+	table: 'Near_Earth_Objects',
+	filePath: path.join(__dirname, '../data/near_earth_objects.csv'),
 	batchSize: 500,
-	headers: {
-		object: 'Object',
-    date: 'Close-Approach (CA) Date',
-    nominalDistance: 'CA DistanceNominal (km)',
-    minDistance: 'CA DistanceMinimum (km)',
-    relVelocity: 'V relative(km/s)',
-    infVelocity: 'V infinity(km/s)',
-    mag: 'H(mag)',
-    diameter: 'Diameter',
-    rarity: 'Rarity'
-	}
+	query: `INSERT INTO Near_Earth_Objects  
+	(spkid, name, earth_moid_ld, magnitude, rotation_hours, pot_hazardous_asteroid) 
+	VALUES ?`
 }
 
-async function populateDB() {
+const SECONDARY = {
+	table: 'Close_Approaches',
+	filePath: path.join(__dirname, '../data/close_approaches.csv'),
+	batchSize: 500,
+	query: `INSERT INTO Close_Approaches  
+	(spkid, date, nominal_distance_km, minimum_distance_km, relative_velocity_km_s, rarity) 
+	VALUES ?`
+}
+
+const TERTIARY = {
+	table: 'Orbits',
+	filePath: path.join(__dirname, '../data/orbits.csv'),
+	batchSize: 500,
+	query: `INSERT INTO Orbits 
+	(spkid, orbital_class, eccentricity, years) VALUES ?`
+}
+
+async function seed() {
 	const connection = await db.getConnection()
 	try {
 		await connection.beginTransaction()
-		await connection.query(`DELETE FROM ${config.table}`) // Prevent duplicated data
-		await streamData(connection)
+		await emptyTables(connection)  // Prevent duplicates
+		await streamPrimary(connection)
+		await streamSecondary(connection)
+		await streamTertiary(connection)
 		await connection.commit()
 		console.log('CSV file processed successfully')
 	} catch (error) {
 		await connection.rollback()
 		console.error('Error populating database: ', error.message)
+		process.exit(1)
 	} finally {
 		connection.release()
-		process.exit()
+		process.exit(0)
 	}
 }
 
-async function streamData(connection) {
-	const batch = []
-	const bulkQuery = `
-	INSERT INTO Close_Approaches 
-	(neo_name, ca_date, nominal_distance_km, minimum_distance_km, 
-	relative_velocity_km_s, infinity_velocity_km_s, abs_magnitude, 
-	diameter_min_km, diameter_max_km, rarity) 
-	VALUES ?`
+async function emptyTables(connection) {
+	await connection.query(`DELETE FROM ${TERTIARY.table}`)
+	await connection.query(`DELETE FROM ${SECONDARY.table}`)
+	// Must be last because other tables have FKs that reference it
+	await connection.query(`DELETE FROM ${PRIMARY.table}`)
+}
 
-	const fileStream = createReadStream(config.filePath)
+async function streamPrimary(connection) {
+	const batch = []
+	const fileStream = createReadStream(PRIMARY.filePath)
 		.pipe(csvParser())
 
 	for await (const row of fileStream) {
-		const values = getValues(row)
+		const values = primaryValues(row)
 		batch.push(values)
-		if (batch.length >= config.batchSize) {
-			await connection.query(bulkQuery, [batch])
+		if (batch.length >= PRIMARY.batchSize) {
+			await connection.query(PRIMARY.query, [batch])
 			batch.length = 0
 		}
 	}
 	if (batch.length > 0) {  // If there are remaining values
-		await connection.query(bulkQuery, [batch])
+		await connection.query(PRIMARY.query, [batch])
 	}
 }
 
-function getValues(row) {
-	const formattedDate = formatDate(row[config.headers.date])
-	const { minDiameter, maxDiameter } = splitDiameter(row[config.headers.diameter])
+const primaryValues = (row) => [
+	row['spkid'],
+	formatName(row['full_name']),
+	row['moid_ld'],
+	safeFloat(row['H']),
+	safeFloat(row['rot_per']),
+	parsePHA(row['pha']),
+]
 
-	const values = [
-		row[config.headers.object],
-		formattedDate,
-		safeFloat(row[config.headers.nominalDistance]),
-		safeFloat(row[config.headers.minDistance]),
-		safeFloat(row[config.headers.relVelocity]),
-		safeFloat(row[config.headers.infVelocity]),
-		safeFloat(row[config.headers.mag]),
-		safeFloat(minDiameter),
-		safeFloat(maxDiameter),
-		safeInt(row[config.headers.rarity])
-	]
+function formatName(value) {
+	return value.trimStart()
+}
 
-	return values
+function parsePHA(value) {
+	return value === 'Y' ? true : false
+}
+
+async function streamSecondary(connection) {
+	const batch = []
+	const spkidMap = await mapSpkid(connection)
+	const fileStream = createReadStream(SECONDARY.filePath)
+		.pipe(csvParser())
+
+	for await (const row of fileStream) {
+		const spkid = spkidMap.get(row['Object'])
+		if (!spkid) continue
+
+		const values = secondaryValues(row, spkid)
+		batch.push(values)
+		if (batch.length >= SECONDARY.batchSize) {
+			await connection.query(SECONDARY.query, [batch])
+			batch.length = 0
+		}
+	}
+	if (batch.length > 0) {  // If there are remaining values
+		await connection.query(SECONDARY.query, [batch])
+	}
+}
+
+const secondaryValues = (row, spkid) => [
+	spkid,
+	formatDate(row['Close-Approach (CA) Date']),
+	safeFloat(row['CA DistanceNominal (km)']),
+	safeFloat(row['CA DistanceMinimum (km)']),
+	safeFloat(row['V relative(km/s)']),
+	safeInt(row['Rarity'])
+]
+
+async function mapSpkid(connection) {
+	const [rows] = await connection.query(
+		`SELECT spkid, name FROM Near_Earth_Objects`)
+	
+	const map = new Map()
+	for (const row of rows) {
+		map.set(row.name, row.spkid)
+	}
+	
+	return map
 }
 
 function formatDate(date) {
@@ -99,30 +151,39 @@ function formatDate(date) {
 	return formattedDate
 }
 
-function splitDiameter(diameter) {
-	const parsedDiameter = diameter.replace('m', '')
+async function streamTertiary(connection) {
+	const batch = []
+	const fileStream = createReadStream(TERTIARY.filePath)
+		.pipe(csvParser())
 
-	let minDiameter, maxDiameter
-	if (parsedDiameter.includes('±')) {
-		[minDiameter, maxDiameter] = parsedDiameter.split('±')
-	} else if (parsedDiameter.includes('-')) {
-		[minDiameter, maxDiameter] = parsedDiameter.split('-')
-	} else {
-		minDiameter = parsedDiameter
-		maxDiameter = parsedDiameter
+	for await (const row of fileStream) {
+		const values = tertiaryValues(row)
+		batch.push(values)
+		if (batch.length >= TERTIARY.batchSize) {
+			await connection.query(TERTIARY.query, [batch])
+			batch.length = 0
+		}
 	}
-
-	return { minDiameter, maxDiameter }
+	if (batch.length > 0) {  // If there are remaining values
+		await connection.query(TERTIARY.query, [batch])
+	}
 }
 
-const safeFloat = (val) => {
+const tertiaryValues = (row) => [
+	row['spkid'],
+	row['class'],
+	safeFloat(row['e']),
+	safeFloat(row['per_y']),
+]
+
+function safeFloat(val) {
 	const parsedFloat = parseFloat(val)
 	return Number.isNaN(parsedFloat) ? null : parsedFloat
 }
 
-const safeInt = (val) => {
+function safeInt(val) {
 	const parsedInt = parseInt(val)
 	return Number.isNaN(parsedInt) ? null : parsedInt
 }
 
-populateDB().catch(console.error)
+seed().catch(console.error)
