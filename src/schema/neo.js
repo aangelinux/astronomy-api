970f9cf1a9b3f2afe7ded56b4400c06f039b2e88
Angelina Larsson <al227bn@student.lnu.se>
2026-02-27 00:02:38 +0100

    Add mutations to Neo schema

--- src/schema/neo.js
+++ src/schema/neo.js
@@ -5,12 +5,39 @@
 import { gql } from '@apollo/server'
 
 export const neo = gql`
-	type NEO {
+	type Neo {
 		id: ID!
 		name: String!
 		earth_moid_ld: Float!
 		magnitude: Float
 		rotation_hours: Float
 		pha: Boolean
+	}
+
+	type DeleteNeoResponse {
+		deletedNeo: Neo
+		message: String
+	}
+
+	extend type Mutation {
+		addNeo(
+			id: ID!
+			name: String
+			earth_moid_ld: Float
+			magnitude: Float
+			rotation_hours: Float
+			pha: Boolean
+		): Neo
+
+		updateNeo(
+			id: ID!
+			name: String
+			earth_moid_ld: Float
+			magnitude: Float
+			rotation_hours: Float
+			pha: Boolean
+		): Neo
+
+		deleteNeo(id: ID!): DeleteNeoResponse
 	}
 `
