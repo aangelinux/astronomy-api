9ae867e396056c7df103596b615067abfedae68b
Angelina Larsson <al227bn@student.lnu.se>
2026-02-27 00:19:35 +0100

    Add class to models/neo

--- src/models/neo.js
+++ src/models/neo.js
@@ -2,12 +2,35 @@
  * Queries to the Near-Earth Objects table.
  */
 
+import db from '../config/db.js'
+
 export default class Neo {
 	static async getNeo(id) {
 
 	}
 
 	static async getAllNeos() {
+		const query = `
+		SELECT * FROM Near_Earth_Objects
+		WHERE spkid > 50
+		ORDER BY spkid
+		LIMIT 50`
+
+		const [result] = await db.query(query)
+		console.log(result)
+		
+		return result
+	}
+
+	static async addNeo(data) {
+
+	}
+
+	static async updateNeo(data) {
+
+	}
+
+	static async deleteNeo(id) {
 
 	}
 }
