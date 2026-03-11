232ee16dff489070d03deaba6bcfe2b1f0fab759
Angelina Larsson <al227bn@student.lnu.se>
2026-03-11 14:19:00 +0100

    Change loadFilesSync in schema/index to use loadFiles

--- src/schema/index.js
+++ src/schema/index.js
@@ -3,14 +3,14 @@
  */
 
 import { mergeTypeDefs } from '@graphql-tools/merge'
-import { loadFilesSync } from '@graphql-tools/load-files'
+import { loadFiles } from '@graphql-tools/load-files'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const types = loadFilesSync(path.join(__dirname, '*.schema.js'),)
+const types = loadFiles(path.join(__dirname, '*.schema.js'),)
 
 /**
  * Checks if each module is a GraphQL schema,
