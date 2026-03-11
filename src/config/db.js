790e089b43cf26a4a9ced576fb338c1ea7da8121
Angelina Larsson <al227bn@student.lnu.se>
2026-03-11 15:15:41 +0100

    Update DB connection

--- src/config/db.js
+++ src/config/db.js
@@ -8,10 +8,16 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const db = mysql.createPool({
+	port: process.env.DB_PORT,
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME
+})
+
+db.query('SHOW TABLES', (error, results) => {
+  if (error) throw error
+  console.log(results)
 })
 
 export default db
