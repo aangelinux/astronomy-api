f594b808184935952d11934b9bd970199d7bbeef
Angelina Larsson <al227bn@student.lnu.se>
2026-03-11 15:51:05 +0100

    Add env names

--- src/config/db.js
+++ src/config/db.js
@@ -8,11 +8,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const db = mysql.createPool({
-	port: process.env.DB_PORT,
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME
+	port: process.env.MYSQLPORT,
+	host: process.env.MYSQLHOST,
+	user: process.env.MYSQLUSER,
+	password: process.env.MYSQLPASSWORD,
+	database: process.env.MYSQLDATABASE
 })
 
 export default db
