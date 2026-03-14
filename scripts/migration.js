ea9cb6e576a3e29a2ea462d3b73dbfd4d6c821db
Angelina Larsson <al227bn@student.lnu.se>
2026-03-14 12:55:31 +0100

    Update migration script

--- scripts/migration.js
+++ scripts/migration.js
@@ -4,52 +4,67 @@
 
 import db from '../src/config/db.js'
 
-const astronomyQuery = `USE astronomy_api`
-await db.query(astronomyQuery)
+async function neoSchema() {
+	const query = `
+		CREATE TABLE near_earth_objects (
+			spkid INT NOT NULL PRIMARY KEY,
+			name VARCHAR(255) NOT NULL,
+			earth_moid_ld FLOAT NOT NULL,
+			magnitude FLOAT,
+			rotation_hours FLOAT,
+			pot_hazardous_asteroid BOOLEAN
+		)`
 
-const neoQuery = `
-	CREATE TABLE near_earth_objects (
-		spkid INT NOT NULL PRIMARY KEY,
-		name VARCHAR(255) NOT NULL,
-		earth_moid_ld FLOAT NOT NULL,
-		magnitude FLOAT,
-		rotation_hours FLOAT,
-		pot_hazardous_asteroid BOOLEAN
-	)`
+	const [result] = await db.query(query)
+	console.log(result)
+}
 
-await db.query(neoQuery)
+async function orbitSchema() {
+	const query = `
+		CREATE TABLE orbits (
+			spkid INT NOT NULL PRIMARY KEY,
+			orbital_class VARCHAR(3) NOT NULL,
+			eccentricity FLOAT,
+			years FLOAT,
+			FOREIGN KEY (spkid) REFERENCES near_earth_objects(spkid)
+		)`
 
-const orbitQuery = `
-	CREATE TABLE orbits (
-		spkid INT NOT NULL PRIMARY KEY,
-		orbital_class VARCHAR(3) NOT NULL,
-		eccentricity FLOAT,
-		years FLOAT,
-		FOREIGN KEY (spkid) REFERENCES near_earth_objects(spkid)
-	)`
+	const [result] = await db.query(query)
+	console.log(result)
 
-await db.query(orbitQuery)
+}
 
-const approachQuery = `
-	CREATE TABLE close_approaches (
-		id INT AUTO_INCREMENT PRIMARY KEY,
-		spkid INT NOT NULL,
-		date DATETIME NOT NULL,
-		nominal_distance_km FLOAT NOT NULL,
-		minimum_distance_km FLOAT NOT NULL,
-		relative_velocity_km_s FLOAT,
-		rarity INT,
-		FOREIGN KEY (spkid) REFERENCES near_earth_objects(spkid)
-	)`
+async function approachSchema() {
+	const query = `
+		CREATE TABLE close_approaches (
+			id INT AUTO_INCREMENT PRIMARY KEY,
+			spkid INT NOT NULL,
+			date DATETIME NOT NULL,
+			nominal_distance_km FLOAT NOT NULL,
+			minimum_distance_km FLOAT NOT NULL,
+			relative_velocity_km_s FLOAT,
+			rarity INT,
+			FOREIGN KEY (spkid) REFERENCES near_earth_objects(spkid)
+		)`
 
-await db.query(approachQuery)
+	const [result] = await db.query(query)
+	console.log(result)
+}
 
-const userQuery = `
-	CREATE TABLE users (
-		userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		username VARCHAR(100) UNIQUE NOT NULL,
-		password_hash VARCHAR(255) NOT NULL,
-		created DATETIME
-	)`
+async function userSchema() {
+	const query = `
+		CREATE TABLE users (
+			userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			username VARCHAR(100) UNIQUE NOT NULL,
+			password_hash VARCHAR(255) NOT NULL,
+			created DATETIME
+		)`
 
-await db.query(userQuery)
+	const [result] = await db.query(query)
+	console.log(result)
+}
+
+await neoSchema()
+await orbitSchema()
+await approachSchema()
+await userSchema()
