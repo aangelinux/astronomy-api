bff7328b5d3bd22b5a2275171c88d127b67e7323
Angelina Larsson <al227bn@student.lnu.se>
2026-03-22 18:51:09 +0100

    Refactor seed script

---
+++ scripts/datasetConfig.js
@@ -0,0 +1,73 @@
+/**
+ * Stores configuration objects for the datasets.
+ */
+
+import { fileURLToPath } from 'url'
+import path from 'path'
+import * as format from './formattingUtils.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export const NEOS = {
+	filePath: path.join(__dirname, '../data/near_earth_objects.csv'),
+	table: 'near_earth_objects',
+	query: 
+	`INSERT INTO near_earth_objects  
+	(spkid, name, earth_moid_ld, magnitude, rotation_hours, pot_hazardous_asteroid) 
+	VALUES ?`,
+
+	/**
+	 * Extract CSV data and modify it where needed,
+	 * so it can be safely inserted into the DB
+	 */
+ 	transform: (row) => [  
+		row['spkid'],
+		format.name(row['full_name']),
+		format.float(row['moid_ld']),
+		format.float(row['H']),
+		format.float(row['rot_per']),
+		format.pha(row['pha']),
+	]
+}
+
+export const ORBITS = {
+	filePath: path.join(__dirname, '../data/orbits.csv'),
+	table: 'orbits',
+	query: 
+	`INSERT INTO orbits 
+	(spkid, orbital_class, eccentricity, years) VALUES ?`,
+
+	transform: (row) => [
+		row['spkid'],
+		row['class'],
+		format.float(row['e']),
+		format.float(row['per_y']),
+	]
+}
+
+export const APPROACHES = {
+	filePath: path.join(__dirname, '../data/close_approaches.csv'),
+	table: 'close_approaches',
+	query: 
+	`INSERT INTO close_approaches  
+	(spkid, date, nominal_distance_km, minimum_distance_km, relative_velocity_km_s, rarity) 
+	VALUES ?`,
+
+  transform: (row, { spkids }) => {
+		// The file of Approaches only has the names of the NEOs,
+		// not their spkids (unique IDs), so before inserting spkid,
+		// we have to get it from the NEO dataset
+		const spkid = spkids.get(row['Object'])
+		if (!spkid) return null
+
+		return [
+			spkid,
+			format.date(row['Close-Approach (CA) Date']),
+			format.float(row['CA DistanceNominal (km)']),
+			format.float(row['CA DistanceMinimum (km)']),
+			format.float(row['V relative(km/s)']),
+			format.int(row['Rarity'])
+		]
+	} 
+}
--- scripts/seed.js
+++ scripts/seed.js
@@ -5,76 +5,9 @@
 import db from '../src/config/db.js'
 import csvParser from 'csv-parser'
 import { createReadStream } from 'fs'
-import { fileURLToPath } from 'url'
-import path from 'path'
-import * as format from './formattingUtils.js'
+import { NEOS, ORBITS, APPROACHES } from './datasetConfig.js'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 const batchSize = 500
-
-const NEOS = {
-	filePath: path.join(__dirname, '../data/near_earth_objects.csv'),
-	table: 'near_earth_objects',
-	query: 
-	`INSERT INTO near_earth_objects  
-	(spkid, name, earth_moid_ld, magnitude, rotation_hours, pot_hazardous_asteroid) 
-	VALUES ?`,
-
-	/**
-	 * Extract CSV data and modify it where needed,
-	 * so it can be safely inserted into the DB
-	 */
- 	transform: (row) => [  
-		row['spkid'],
-		format.name(row['full_name']),
-		format.float(row['moid_ld']),
-		format.float(row['H']),
-		format.float(row['rot_per']),
-		format.pha(row['pha']),
-	]
-}
-
-const ORBITS = {
-	filePath: path.join(__dirname, '../data/orbits.csv'),
-	table: 'orbits',
-	query: 
-	`INSERT INTO orbits 
-	(spkid, orbital_class, eccentricity, years) VALUES ?`,
-
-	transform: (row) => [
-		row['spkid'],
-		row['class'],
-		format.float(row['e']),
-		format.float(row['per_y']),
-	]
-}
-
-const APPROACHES = {
-	filePath: path.join(__dirname, '../data/close_approaches.csv'),
-	table: 'close_approaches',
-	query: 
-	`INSERT INTO close_approaches  
-	(spkid, date, nominal_distance_km, minimum_distance_km, relative_velocity_km_s, rarity) 
-	VALUES ?`,
-
-  transform: (row, { spkids }) => {
-		// The file of Approaches only has the names of the NEOs,
-		// not their spkids (unique IDs), so before inserting spkid,
-		// we have to get it from the NEO dataset
-		const spkid = spkids.get(row['Object'])
-		if (!spkid) return null
-
-		return [
-			spkid,
-			format.date(row['Close-Approach (CA) Date']),
-			format.float(row['CA DistanceNominal (km)']),
-			format.float(row['CA DistanceMinimum (km)']),
-			format.float(row['V relative(km/s)']),
-			format.int(row['Rarity'])
-		]
-	} 
-}
 
 async function seed() {
 	const connection = await db.getConnection()
@@ -87,9 +20,10 @@ async function seed() {
 			{ spkids: await getSpkids(connection) })
 		await connection.commit()
 
-		console.log('CSV files processed successfully')
+		console.log('CSV files processed successfully!')
 	} catch (error) {
 		await connection.rollback()
+		console.error('Error populating DB: ', error.message)
 	} finally {
 		connection.release()
 	}
@@ -101,9 +35,9 @@ async function seed() {
 async function emptyDatabase(connection) {
 	await connection.query(`DELETE FROM ${ORBITS.table}`)
 
-	// Must be TRUNCATE, not DELETE, to reset 'id' to 0
+	// Must be TRUNCATE, not DELETE, to reset id to 1
 	await connection.query(`TRUNCATE TABLE ${APPROACHES.table}`)
-	
+
 	// Must be last because the other tables reference it
 	await connection.query(`DELETE FROM ${NEOS.table}`)
 }
@@ -149,7 +83,4 @@ async function getSpkids(connection) {
 
 seed()
 	.then(() => process.exit(0))
-	.catch((error) => {
-		console.error('Error populating database: ', error.message)
-		process.exit(1)
-	})
+	.catch((e) => process.exit(1))
